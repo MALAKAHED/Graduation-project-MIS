fa1756f73cc9b010f9cfae9647eaa7384dfb6b08
islamsaeid100 <ghgg12312323@gmail.com>
2026-04-06 18:49:24 +0200

    ss1

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // تأكد إن ده الـ URL اللي زميلك مشغل عليه السيرفر (غالباً 8000)
-    baseURL: 'http://127.0.0.1:8000/' 
+    // ده عنوان الباك-إند (المخ) اللي شغال على 8000
+    baseURL: 'http://127.0.0.1:8000/api/', 
 });
 
-// "الإنترسيبتور": وظيفتة يحط مفتاح الدخول (Token) في رأس كل طلب (Headers)
+// إضافة الـ Token تلقائياً لأي طلب (عشان م يرفضش الدخول)
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('access_token');
     if (token) {
